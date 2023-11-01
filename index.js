5f5bf1125cfeef86b4baa6307e6e30ccf9aeed91
Nick Weiland <njweiland23@gmail.com>
2023-11-01 16:35:19 -0500

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Code your solution here
+function findMatching(drivers, searchName) {
+   return drivers.filter(driver => driver.toLowerCase() === searchName.toLowerCase())
+}
+function fuzzyMatch(drivers,searchName) {
+    return drivers.filter(driver => driver.slice(0,2) === searchName.slice(0,2))
+}
+function matchName(drivers, town) {
+    return drivers.filter(driver => driver.name === town)
+}
